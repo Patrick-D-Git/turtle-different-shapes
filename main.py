24536515b2ef93be24b7f37cc8fdf06d5268438a
Patrick D <patrick.montejo@gmail.com>
2024-02-16 23:13:52 -0500

    updated codes and created a draw shape function

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ screen = Screen()  # Screen class
 
 colors = ["red", "green", "blue", "orange", "purple", "yellow", "violet", "gray"]
 
-for line in range(3, 11):
-    angle = 360 / int(line)
-    random_color = random.choice(colors)
-    tim.pencolor(random_color)
 
-    for shape in range(line):
+def draw_shape(n_sides):
+    """"""
+    angle = 360 / int(n_sides)
+    for _ in range(n_sides):
         tim.forward(100)
         tim.left(angle)
+
+
+for lines in range(3, 11):
+    random_color = random.choice(colors)
+    tim.pencolor(random_color)
+    draw_shape(lines)
+
 
 screen.exitonclick()  # Shows the white box
